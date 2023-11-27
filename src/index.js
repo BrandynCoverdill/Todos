3936e165be66228c77da3c930e18ec1686a08eb2
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-11-27 03:39:36 -0700

    Added feature to view todo details.
    If you click on the todo, a modal will popup showing the details
    for that todo.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import {
 	createProject,
 	editProject,
 } from './Interface';
-// import Modal from './Modal'; // TODO Remove this when finished with modal
 
 // Show the UI of the app
 document.body.appendChild(userInterface());
-// document.body.appendChild(Modal('create')); // TODO Remove this when finished with modal
 
 // Global variables
 const projects = document.querySelectorAll('.project-table > tr');
